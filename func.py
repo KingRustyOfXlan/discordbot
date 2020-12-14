a836e723eedc4485a2d0142a38dd033f1e742212
KingRustyOfXlan <kingrustyofxlan@rusty-hp-laptop-Archlinux.localdomain>
2020-12-14 16:32:57 -0500

    Added comments (test for authorizer/commiter)

--- func.py
+++ func.py
@@ -8,6 +8,7 @@ def terminalOutput(output):
 
 ### Roll die/dice
 async def roll(msg, number):
+    # String for roll results
     rolls = ""
     # Looks for roll multiplier
     if number.find('*') != -1:
@@ -18,6 +19,7 @@ async def roll(msg, number):
         # Caps die and multiplier at 100 so the bot doesn't die
         if dieNumber > 100 or multiplier > 100:
             terminalOutput('Roll:\n' + '    Error: roll die cap')
+            # Send an embedded message telling user not to roll as much
             await msg.channel.send(embed=discord.Embed(
                 title = "{0.author.name}".format(msg),
                 description = "Die or multiplier too large, cap is 100",
@@ -29,26 +31,27 @@ async def roll(msg, number):
                 ))
             return # Do nothing
 
-        # Calls RNG and displays results
+        # Calls RNG
         for i in range(0, multiplier):
             # Run RNG to find result
             rollValue = random.randrange(0, int(dieNumber)) + 1
+            # Add the result of the RNG call to the result string
             if i == multiplier - 1:
                 rolls = rolls + '{}'.format(rollValue)
             else:
                 rolls = rolls + '{}, '.format(rollValue)
 
-    # Calls RNG and displays results
+    # Calls RNG
     else:
         # Run RNG to find result
         rollValue = random.randrange(0, int(number)) + 1
-        rolls = rolls + '{}'.format(rollValue)
-    
+        rolls = rolls + '{}'.format(rollValue) 
     terminalOutput(
         'Roll:\n    User:"{0.author.name}"'.format(msg) +
         '   Die: "{}"'.format(str(number)) +
         '   Result: "{}"'.format(str(rolls))
         )
+    # Sends an embedded message containing the results of the roll
     await msg.channel.send(embed=discord.Embed(
         title = "{0.author.name}: Roll Result".format(msg),
         description = rolls,
